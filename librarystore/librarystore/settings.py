3a555e5d358ad09cea536552a36877816dd8c5d1
sebica98 <sebyursan@gmail.com>
2022-06-10 09:26:57 +0300

    Finished profile page and restructured files

--- librarystore/librarystore/settings.py
+++ librarystore/librarystore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'orders',
     'librarystore_app',
     'kart',
